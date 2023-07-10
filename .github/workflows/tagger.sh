e9ea754f64e4e6eb4253c3a6208515b61d6ce1e3
orhtej2 <2871798+orhtej2@users.noreply.github.com>
2023-07-10 18:15:34 +0200

    Create tagger.sh

---
+++ .github/workflows/tagger.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+toml_to_json() {
+    python3 -c 'import toml, json, sys; print(json.dumps(toml.load(sys.stdin)))'
+}
+
+if [ -f manifest.json ];
+then
+  current_version=$(cat manifest.json | jq -j '.version | sed -e 's/~/-/')
+elif [ -f manifest.toml ];
+then
+  current_version=$(cat manifest.toml | toml_to_json | jq ".version" | sed -e 's/~/-/')
+else
+  echo No manifest found!
+  exit 0
+fi
+
+echo "PROCEED=true" >> $GITHUB_ENV
+echo "VERSION=$current_version" >> $GITHUB_ENV
